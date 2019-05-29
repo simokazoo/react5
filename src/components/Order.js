c9efabcfbe2c4ec8badf701b89a271b0290eb36a
simokazoo <french.simona@yahoo.com>
2019-05-29 12:19:34 +0200

    deploy

---
+++ src/components/Order.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Order extends React.Component {
+
+    render() {
+      return (
+        <div className="order">
+          <ul>
+            <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+            <li>{this.props.size}</li>
+            <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+            <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  Order.defaultProps = {
+    cone:  true,
+    size: 'regular' ,
+    scoops: ['lemon', 'chocolate', 'vanilla', 'strawberry'],
+    orderInfo: {
+        customerName: 'Ali',
+        orderedAt: 3
+    }
+  };
+
+  Order.propTypes = {
+      cone: PropTypes.bool,
+      size: PropTypes.string,
+      scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+      orderInfo: PropTypes.shape({
+          customerName: PropTypes.string.isRequired,
+          orderedAt: PropTypes.number.isRequired
+      }).isRequired
+  };
